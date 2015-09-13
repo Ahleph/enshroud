8978901e754fcbe7050a8afc7379071ef7b601dd
aleeph <aleeph4@gmail.com>
2015-09-12 23:18:33 -0500

    first commit

---
+++ swaptest.js
@@ -0,0 +1,6 @@
+// Checking page title
+if (document.title.indexOf("Facebook") != -1) {
+    // Creating Elements
+    var textswap = "HACKED"
+    var testreplace = document.getElementById(".1m.1:$mid=11442016327895=252bd9e00430ad6f822.2:0.0.0.0.0.0")
+    testreplace.content=textswap
